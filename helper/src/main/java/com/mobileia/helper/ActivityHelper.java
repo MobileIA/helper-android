54a284fa3d59c38eb7362f9d7133d25ab81ca475
Matias Camiletti <matias.camiletti@gmail.com>
2017-08-03 22:44:54 -0300

    Agregado funcionalidad para obtener los parametros Extras de un intent.

--- helper/src/main/java/com/mobileia/helper/ActivityHelper.java
+++ helper/src/main/java/com/mobileia/helper/ActivityHelper.java
@@ -17,4 +17,13 @@ public class ActivityHelper {
     public static void createInstance(Activity context, Intent intent){
         context.startActivity(intent);
     }
+
+    public static Object getExtraIntent(Activity context, String key){
+        // Verificamos que haya parametros
+        if (context.getIntent().getExtras() == null || context.getIntent().getExtras().isEmpty()) {
+            return null;
+        }
+        // Retornamos el valor buscado
+        return context.getIntent().getExtras().get(key);
+    }
 }
